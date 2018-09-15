ee5a5a8327c7b32e422835ad81865d8c2383b8d2
MekhailMaged96 <mekhailmaged45@gmail.com>
2018-09-15 11:52:26 +0200

    add fake data using Factories

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,16 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\Products;
 $factory->define(App\Review::class, function (Faker $faker) {
     return [
-        //
+
+        'product_id'=>function(){
+
+            return Products::all()->random();
+        },
+        'customer' =>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(0,5),
     ];
 });
